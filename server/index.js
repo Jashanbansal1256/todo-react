ebfd71385670695eb77d821026fac91062b0c54e
Jashan Bansal <jashanbansal7861@gmail.com>
2024-05-12 13:10:05 +0530

    complete the todo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-// import Routes from './routes/route.js'
+import Routes from './routes/route.js'
 
 // Here .js is important
 import Connection from './database/db.js';
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
-// app.use('/', Routes);
+app.use('/', Routes);
 
 
 const PORT = 8000;
